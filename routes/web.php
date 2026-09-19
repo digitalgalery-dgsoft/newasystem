e2978e64fcdda42bc100dbeb29d5e6394bce82e1
Jeigray <abdurrahmanjamil.mail@gmail.com>
2026-09-19 15:14:35 +0700

    fix: restore missing master.prinsiple.destroy route in routes/web.php

--- routes/web.php
+++ routes/web.php
@@ -56,6 +56,7 @@ Route::middleware(['admin'])->prefix('master')->name('master.')->group(function 
     Route::post('/prinsiple/import-official', [PrincipleController::class, 'reimportOfficial'])->name('prinsiple.reimport');
     Route::put('/prinsiple/{id}', [PrincipleController::class, 'update'])->name('prinsiple.update');
     Route::get('/prinsiple/{id}/toggle', [PrincipleController::class, 'toggleStatus'])->name('prinsiple.toggle');
+    Route::delete('/prinsiple/{id}', [PrincipleController::class, 'destroy'])->name('prinsiple.destroy');
     // Master Soal Matematika
     Route::get('/math', [\App\Http\Controllers\MathQuestionController::class, 'index'])->name('math.index');
     Route::post('/math', [\App\Http\Controllers\MathQuestionController::class, 'store'])->name('math.store');
